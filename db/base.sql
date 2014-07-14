ac045ed5734020b0753b89ad796dc7713edbbab8
Jax Kodex <jorge12745@hotmail.com>
2014-07-13 20:15:12 -0500

    Highcharts Added

--- db/base.sql
+++ db/base.sql
@@ -71,3 +71,29 @@ insert into seccion values (25, 13, 'B');
 
 
 -- preload clase
+select * from clase;
+insert into clase values (1, 1, 1, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (2, 1, 2, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (3, 1, 3, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (4, 1, 4, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (5, 1, 5, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (6, 1, 6, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (7, 1, 7, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (8, 1, 8, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (9, 1, 9, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (10, 1, 10, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (11, 1, 11, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (12, 1, 12, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (13, 1, 13, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (14, 1, 14, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (15, 1, 15, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (16, 1, 16, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (17, 1, 17, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (18, 1, 18, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (19, 1, 19, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (20, 1, 20, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (21, 1, 21, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (22, 1, 22, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (23, 1, 23, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (24, 1, 24, now(), now(), 1, 0, 0, 0, 0);
+insert into clase values (25, 1, 25, now(), now(), 1, 0, 0, 0, 0);
